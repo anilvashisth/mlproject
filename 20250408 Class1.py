352235ceacc3a3e8d6db0ccd3bae4262e4b8c344
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2025-04-11 13:58:36 +0530

    Add files via upload

---
+++ 20250408 Class1.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a = 15;
+print(a)
+
+
+# In[2]:
+
+
+b = 20;
+print(b)
+
+
+# In[3]:
+
+
+# Addition of two variable
+c=a+b;
+print(c)
+
+
+# In[4]:
+
+
+# Subtraction 
+d = b-a;
+print (d)
+
+
+# In[5]:
+
+
+# Multiply two variable
+e = a*b;
+print(e)
+
+
+# In[6]:
+
+
+# Division
+f=b/a;
+print(f)
+
+
+# In[7]:
+
+
+#modulo
+g=b%a;
+print(g)
+
+
+# In[8]:
+
+
+#square
+h=a*a;
+print(h)
+
+
+# In[12]:
+
+
+# square 
+a=12;
+h1 =a ** 2;
+print(h1)
+
+
+# In[14]:
+
+
+# cube
+a=3
+h2=a ** 3;
+print(h2)
+
+
+# In[18]:
+
+
+# square root and cube root
+a = 144;
+b = 64;
+print(round (a**(1/2),6))
+
+
+# In[19]:
+
+
+print(round (b**(1/3),6))
+
+
+# In[21]:
+
+
+c =25;
+print(round(c**(1/3),6))
+
+
+# In[ ]:
+
+
+
+
