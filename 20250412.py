53d6f81962c22c549d6b3d6a313c10c7bab28bb8
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2025-04-16 10:03:02 +0530

    Add files via upload
    Python Class 1 on dated 12 April 2025

---
+++ 20250412.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import this
+
+
+# In[2]:
+
+
+print ("Hello Everyone this is Python Program")
+
+
+# In[3]:
+
+
+len("this is my program")
+
+
+# In[4]:
+
+
+a="Anil";
+b=25;
+c=4.52;
+d=5+7j;
+e=True;
+print (a,b,c,d,e)
+
+
+# In[16]:
+
+
+d.imag
+
+
+# In[17]:
+
+
+d.real
+
+
+# In[18]:
+
+
+print(c)
+
+
+# In[11]:
+
+
+type(e)
+
+
+# In[14]:
+
+
+c.real
+
+
+# In[15]:
+
+
+c.imag
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
